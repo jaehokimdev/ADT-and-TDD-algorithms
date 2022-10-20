c1567ec22579e96f10becab2e2f3249d9c60acb2
jaehokimdev <101899896+jaehokimdev@users.noreply.github.com>
2022-10-19 19:07:03 -0600

    working on now

--- src/utilities/QueueADT.java
+++ src/utilities/QueueADT.java
@@ -1,34 +1,102 @@
+/**
+ * 
+ */
 package utilities;
 
+import java.util.NoSuchElementException;
+
+/**
+ * @author valentinmorales
+ *
+ */
+
+/**
+ * <p>
+ * The <code>queueADT</code> interface is designed to be used as a basis for assignment 2 part 1 for  CPRG 311 class at 
+ * SAIT. The implementors of this interface will be required to add all the 
+ * functionality.
+ * </p>
+ * 
+ * @param <E> The type of elements this queue holds.
+ */
 public interface QueueADT<E> {
 	
 	/**
 	 * The size method will return the current element count contained 
-	 * in the list.
+	 * in the Queue.
+	 * 
+	 * Precondition: A valid Queue exists.
 	 * 
 	 * @return The current element count.
 	 */
 	public int size();
 	
 	/**
-	 * Removes all of the elements from this list. This list will be empty after
-	 * this call returns.
-	 */
-	public void clear();
-	
-	/**
-	 * @return <code>true</code> if this list contains no elements.
+	 * Returns <code>true</code> if this Queue contains no elements.
+	 * 
+	 * Precondition: A valid Queue exists.
+	 * 
+	 * @return <code>true</code> if this Queue contains no elements.
 	 */
 	public boolean isEmpty();
 	
+	/**
+	 * Removes all of the elements from this Queue. This Queue will be empty after
+	 * this call returns.
+	 * 
+	 * Precondition: A valid Queue exists.
+	 * 
+	 */
+	public void clear();
 	
-	public void enqueue(E toAdd) throws NullPointerException;
+	
+	/**
+	 * Append the specified element to the end of this Queue.
+	 * Implementations that support this operation may place limitations 
+	 * on what elements may be appended to this queue. In particular, some
+	 * implementations will refuse to push <code>null</code> elements.
+	 * List classes should clearly specify in their
+	 * documentation any restrictions on what elements may be added.
+	 * 
+	 * Precondition: A valid Queue exists.
+	 * 
+	 * Postcondition: An element E is appended to the queue and its depth is incremented
+	 * 
+	 * @param toEnqueue
+	 * 			Element to be appended to this queue.
+	 * @return true if element is appended successfully.
+	 * @throws NullPointerException
+	 * 			If the specified element is <code>null</code> and the queue
+	 * 			implementation does not support having <code>null</code>
+	 * 			elements.
+	 */
+	public boolean enqueue(E toEnqueue) throws NullPointerException;
 	
 	
-	public E dequeue();
+	
+	/**
+	 * Removes the element at the start of this queue.
+	 * Returns the element that was removed from the queue.
+	 * 
+	 * Precondition: A valid Queue exists.
+	 * 
+	 * Postcondition: An element E is removed from the queue and its size is decremented
+	 * 
+	 * @return The removed element.
+	 * @throws IndexOutOfBoundsException
+	 * 			If the element in the queue does not exist.
+	 */
+	public E dequeue() throws NoSuchElementException;
 	
 	
-	public E peek();
-	
-	
+	/**
+	 * Returns the element at the start of the queue.
+	 * 
+	 * Precondition: A valid Queue exists.
+	 * 
+	 * @return The element at the start of the queue.
+	 * @throws NoSuchElementException
+	 * 			If the element in the queue does not exist
+	 */
+	public E peek()throws NoSuchElementException;
 }
--- src/utilities/StackADT.java
+++ src/utilities/StackADT.java
@@ -1,49 +1,104 @@
+/**
+ * 
+ */
 package utilities;
 
+import java.util.EmptyStackException;
+
+/**
+ * @author valentinmorales
+ *
+ */
+
+/**
+ * <p>
+ * The <code>stackADT</code> interface is designed to be used as a basis for assignment 2 part 1 for  CPRG 311 class at 
+ * SAIT. The implementors of this interface will be required to add all the 
+ * functionality.
+ * </p>
+ * 
+ * @param <E> The type of elements this stack holds.
+ */
 public interface StackADT<E> {
 	
 	/**
 	 * The size method will return the current element count contained 
-	 * in the list.
+	 * in the stack.
+	 * 
+	 * Precondition: A valid stack exists.
 	 * 
 	 * @return The current element count.
 	 */
 	public int size();
 	
 	/**
-	 * Removes all of the elements from this list. This list will be empty after
-	 * this call returns.
-	 */
-	public void clear();
-	
-	/**
-	 * @return <code>true</code> if this list contains no elements.
+	 * Returns <code>true</code> if this stack contains no elements.
+	 * 
+	 * Precondition: A valid stack exists.
+	 * 
+	 * @return <code>true</code> if this stack contains no elements.
 	 */
 	public boolean isEmpty();
 	
 	/**
-	 * Appends the specified element to the top of this list.
+	 * Removes all of the elements from the stack. This stack will be empty after
+	 * this call returns.
+	 * 
+	 * Precondition: A valid stack object exists.
+	 */
+	public void clear();
+	
+	
+	
+	/**
+	 * Push the specified element to the top of this stack.
 	 * Implementations that support this operation may place limitations 
-	 * on what elements may be added to this list. In particular, some
-	 * implementations will refuse to add <code>null</code> elements.
+	 * on what elements may be added to this stack. In particular, some
+	 * implementations will refuse to push <code>null</code> elements.
 	 * List classes should clearly specify in their
 	 * documentation any restrictions on what elements may be added.
 	 * 
-	 * @param toAdd
-	 * 			Element to be appended to this list.
+	 * Precondition: A valid stack exists.
+	 * 
+	 * Postcondition: An element E is pushed to the stack and its length is incremented.
+	 * 
+	 * @param toPush
+	 * 			Element to be pushed to this stack.
+	 * @return true if element is pushed successfully.
 	 * @throws NullPointerException
-	 * 			If the specified element is <code>null</code> and the list
+	 * 			If the specified element is <code>null</code> and the stack
 	 * 			implementation does not support having <code>null</code>
 	 * 			elements.
 	 */
-	public void push(E toAdd) throws NullPointerException;
+	public boolean push(E toPush) throws NullPointerException;
 	
 	
-	public E pop();
+	/**
+	 * Removes the element at the top of this stack.
+	 * Returns the element that was removed from the stack.
+	 * 
+	 * Precondition: A valid stack exists.
+	 * 
+	 * Postcondition: An element E is removed from the stack and its length is decremented.
+	 * 
+	 * @return The removed element.
+	 * @throws EmptyStackException
+	 * 			If the stack is empty.
+	 */
+	public  E pop() throws EmptyStackException;
 	
 	
-	public E peek();
 	
+	/**
+	 * Returns the element at the top of the stack.
+	 * 
+	 * Precondition: A valid stack exists.
+	 * 
+	 * @return The element at the top of the stack.
+	 * @throws EmptyStackException
+	 * 			If the stack is empty
+	 */
+	public  E peek()throws EmptyStackException;
 	
-	
+
 }
